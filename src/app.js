726b7e9020a1f69f77b35e37e8bcd02c2d5c5d62
andrey klimenok <jaguarislove@gmail.com>
2020-06-04 21:18:51 +0200

    refactoring

--- src/app.js
+++ src/app.js
@@ -6,7 +6,28 @@ import _ from 'lodash';
 import i18next from 'i18next';
 import startWatch from './watchers';
 
-const app = async () => {
+const proxify = (url) => `https://cors-anywhere.herokuapp.com/${url}`;
+
+const parse = (xml) => {
+  const domParser = new DOMParser();
+  const dom = domParser.parseFromString(xml, 'text/xml');
+  const title = dom.querySelector('title').textContent;
+  const description = dom.querySelector('description').textContent;
+  const feed = {
+    title, description,
+  };
+  const postsElements = dom.querySelectorAll('item');
+  const posts = [...postsElements].map((post) => {
+    const postTitle = post.querySelector('title').textContent;
+    const postLink = post.querySelector('link').textContent;
+    return {
+      title: postTitle, link: postLink,
+    };
+  });
+  return { feed, posts };
+};
+
+export default async () => {
   await i18next.init({
     lng: 'en',
     debug: true,
@@ -17,6 +38,7 @@ const app = async () => {
             duplicateEntry: 'Duplicate entry',
             updateNetworkError: 'Error: Update failed. Retrying...',
           },
+          loadedMessage: 'Rss has been loaded',
         },
       },
     },
@@ -25,10 +47,14 @@ const app = async () => {
   const state = {
     form: {
       value: '',
-      error: '',
+      state: 'filling',
     },
     feeds: [],
     posts: [],
+    errors: {
+      validation: null,
+      network: null,
+    },
   };
 
   const elements = {
@@ -37,92 +63,67 @@ const app = async () => {
     rssButton: document.querySelector('button[type="submit"]'),
   };
 
-  const schema = yup.string().url();
 
   const validate = (feeds, value) => {
-    if (feeds.map((feed) => feed.link).includes(value)) {
-      return i18next.t('errors.duplicateEntry');
-    }
+    const feedLinks = feeds.map((feed) => feed.link);
+    const schema = yup.string().notOneOf(feedLinks).url();
     try {
       schema.validateSync(value);
-      return '';
+      return null;
     } catch (e) {
+      if (e.message.includes('this must not be one of')) return i18next.t('errors.duplicateEntry');
       return e.message;
     }
-  };
-
-  const proxify = (url) => `https://cors-anywhere.herokuapp.com/${url}`;
-
-  const getFeed = (url) => axios.get(url);
-  const parse = (xml) => {
-    const domParser = new DOMParser();
-    const dom = domParser.parseFromString(xml, 'text/xml');
-    const title = dom.querySelector('title').textContent;
-    const description = dom.querySelector('description').textContent;
-    const feed = {
-      link: state.form.value, title, description,
-    };
-    const postsElements = dom.querySelectorAll('item');
-    const posts = [...postsElements].map((post) => {
-      const postTitle = post.querySelector('title').textContent;
-      const postLink = post.querySelector('link').textContent;
-      return {
-        title: postTitle, link: postLink,
-      };
-    });
-    return { feed, posts };
   };
 
   const assignFeedId = (object, feedId) => ({ ...object, feedId });
 
   elements.rssInput.addEventListener('input', (e) => {
     state.form.value = e.target.value;
-    state.form.error = validate(state.feeds, state.form.value);
+    state.errors.validation = validate(state.feeds, state.form.value);
+    state.form.state = 'filling';
+    state.errors.network = null;
   });
 
   elements.rssForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    getFeed(proxify(state.form.value))
+    state.form.state = 'processing';
+    axios.get(proxify(state.form.value))
       .then((response) => parse(response.data))
       .then(({ feed, posts }) => {
         const id = _.uniqueId();
-        state.feeds = [...state.feeds, assignFeedId(feed, id)];
+        state.feeds.push(assignFeedId({ ...feed, link: state.form.value }, id));
         const postsWithId = posts.map((post) => assignFeedId(post, id));
-        state.posts = [...postsWithId, ...state.posts];
-        state.form.error = '';
+        state.posts.unshift(...postsWithId);
+        state.errors.network = null;
+        state.form.value = '';
+        state.form.state = 'completed';
       })
       .catch((error) => {
-        state.form.error = error;
+        state.errors.network = error;
+        state.form.state = 'errored';
       });
-    elements.rssInput.value = '';
   });
 
   const checkUpdates = () => {
-    Promise.all(state.feeds.map(
-      (feed) => new Promise(((resolve) => {
-        const fetchedPostsLinks = state.posts
-          .filter((post) => post.feedId === feed.feedId)
-          .map((post) => post.link);
-        getFeed(proxify(feed.link))
-          .then((response) => parse(response.data))
-          .then(({ posts }) => {
-            const newPosts = posts.filter((post) => !fetchedPostsLinks.includes(post.link));
-            const newPostsWithIds = newPosts.map((post) => assignFeedId(post, feed.feedId));
-            state.posts = [...newPostsWithIds, ...state.posts];
-            state.form.error = '';
-            resolve();
-          })
-          .catch(() => {
-            state.form.error = i18next.t('errors.updateNetworkError');
-            resolve();
-          });
-      })),
-    ))
-      .then(() => setTimeout(checkUpdates, 5000));
+    Promise.all(state.feeds.map((feed) => {
+      const fetchedPostsLinks = state.posts
+        .filter((post) => post.feedId === feed.feedId)
+        .map((post) => post.link);
+      return axios.get(proxify(feed.link))
+        .then((response) => parse(response.data))
+        .then(({ posts }) => {
+          const newPosts = posts.filter((post) => !fetchedPostsLinks.includes(post.link));
+          const newPostsWithIds = newPosts.map((post) => assignFeedId(post, feed.feedId));
+          state.posts.unshift(...newPostsWithIds);
+          state.errors.network = null;
+        })
+        .catch(() => {
+          state.errors.network = i18next.t('errors.updateNetworkError');
+        });
+    })).then(() => setTimeout(checkUpdates, 5000));
   };
 
   startWatch(state, elements);
   checkUpdates();
 };
-
-app();
--- src/watchers.js
+++ src/watchers.js
@@ -1,29 +1,71 @@
 import Watchjs from 'melanke-watchjs';
+import _ from 'lodash';
+import i18next from 'i18next';
 
 const startWatch = (state, elements) => {
   const { watch } = Watchjs;
 
-  const renderError = (error) => {
-    const { rssForm } = elements;
-    const errorElement = rssForm.nextElementSibling;
-    if (errorElement) {
+  const renderErrors = (errors) => {
+    const { rssForm, rssInput } = elements;
+    const errorElements = document.querySelectorAll('.text-danger');
+    if (errorElements.length > 0) {
       elements.rssInput.classList.remove('is-invalid');
-      errorElement.remove();
+      errorElements.forEach((errorElement) => errorElement.remove());
     }
-    if (error === '') {
-      return;
-    }
-    elements.rssInput.classList.add('is-invalid');
-    const feedbackElement = document.createElement('div');
-    feedbackElement.classList.add('feedback', 'text-danger');
-    feedbackElement.innerHTML = error;
-    elements.rssForm.after(feedbackElement);
+    errors.forEach((error) => {
+      rssInput.classList.add('is-invalid');
+      const feedbackElement = document.createElement('div');
+      feedbackElement.classList.add('feedback', 'text-danger');
+      feedbackElement.innerHTML = error;
+      rssForm.after(feedbackElement);
+    });
   };
 
-  watch(state.form, 'error', () => {
-    renderError(state.form.error);
-    const rssButton = { elements };
-    rssButton.disabled = !!state.form.error;
+  const renderSuccess = (result) => {
+    const { rssForm } = elements;
+    const successElements = document.querySelectorAll('.text-success');
+    if (successElements.length > 0) {
+      successElements.forEach((errorElement) => errorElement.remove());
+    }
+    if (result === false) {
+      return;
+    }
+    const feedbackElement = document.createElement('div');
+    feedbackElement.classList.add('feedback', 'text-success');
+    feedbackElement.innerHTML = i18next.t('loadedMessage');
+    rssForm.after(feedbackElement);
+  };
+
+  watch(state.form, 'value', () => {
+    const { rssInput } = elements;
+    rssInput.value = state.form.value;
+  });
+
+  watch(state.form, 'state', () => {
+    const { rssButton } = elements;
+    renderSuccess(false);
+    switch (state.form.state) {
+      case 'filling':
+        rssButton.disabled = false;
+        break;
+      case 'processing':
+      case 'errored':
+        rssButton.disabled = true;
+        break;
+      case 'completed':
+        rssButton.disabled = false;
+        renderSuccess(true);
+        break;
+      default:
+        throw new Error(`Not supported form state: ${state.form.state}`);
+    }
+  });
+
+  watch(state, 'errors', () => {
+    const errors = _.values(state.errors).filter((error) => error !== null);
+    renderErrors(errors);
+    const { rssButton } = elements;
+    rssButton.disabled = state.errors.validation !== null;
   });
 
   watch(state, 'feeds', () => {
